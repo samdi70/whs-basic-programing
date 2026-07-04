4576516df79ed1f83cf16ab6bf3124f58a064ac0
junha <junha020607@gmail.com>
2026-07-05 02:27:27 +0900

    add function_type func

--- json_task.c
+++ json_task.c
@@ -30,11 +30,13 @@ int main(void){
     // 2. str에 들어있는 json 텍스트를 json 형식으로 변환
     json_value json = json_create(str);
 
-    // 3. 함수의 개수 추출하기 (과제#1)
+    // 3. 과제 시작 
     json_value ext = json_get(json, "ext");
     int ext_len = json_len(ext);
+
     printf("==================================\n");
     printf("ext length is : %d\n", ext_len);
+    printf("==================================\n");
 
     int count_func = 0;
 
@@ -43,15 +45,31 @@ int main(void){
         char *nodetype = json_get_string(item, "_nodetype");
 
         if (strcmp(nodetype, "FuncDef") == 0){
+            // 함수 개수 올리기!
             count_func++;
+            
+            // 함수의 리턴 타입 출력하기!
+            json_value decl = json_get(item, "decl");
+            char *fname = json_get_string(decl, "name");
+
+            json_value type = json_get(decl, "type");
+            json_value type2 = json_get(type, "type");
+            
+            if (strcmp(json_get_string(type2, "_nodetype"), "PtrDecl") == 0) {
+                type2 = json_get(type2, "type");
+            }
+            json_value type3 = json_get(type2, "type");
+            char *ftype = json_get_string(type3, "names", 0); 
+            
+            printf("%d - functions return type : %s\n", i, ftype);
+            
         }
 
     }
-
-    printf("function counting : %d\n", count_func);
     printf("==================================\n");
-
-
+    // 존재하는 함수의 전체 개수 출력하기
+    printf("all function counting : %d\n", count_func);
+    printf("==================================\n");
 
 
     return 0;
