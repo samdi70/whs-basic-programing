1406c4333610ef3ec7fd47681792c82061a03ff3
junha <junha020607@gmail.com>
2026-07-05 01:45:48 +0900

    add function_counting func

--- json_task.c
+++ json_task.c
@@ -19,7 +19,7 @@ int main(void){
     unsigned long size = ftell(fp);
     fseek(fp, 0, SEEK_SET);
 
-    printf("%lu\n", size);
+    printf("json size is : %lu\n", size);
 
     char *str = (char *)malloc(size + 1);
     fread(str, 1, size, fp);
@@ -30,8 +30,29 @@ int main(void){
     // 2. str에 들어있는 json 텍스트를 json 형식으로 변환
     json_value json = json_create(str);
 
-    // 3. 함수의 개수 추출하기
-    
+    // 3. 함수의 개수 추출하기 (과제#1)
+    json_value ext = json_get(json, "ext");
+    int ext_len = json_len(ext);
+    printf("==================================\n");
+    printf("ext length is : %d\n", ext_len);
+
+    int count_func = 0;
+
+    for (int i=0; i<ext_len; i++){
+        json_value item = json_get(json, "ext", i);
+        char *nodetype = json_get_string(item, "_nodetype");
+
+        if (strcmp(nodetype, "FuncDef") == 0){
+            count_func++;
+        }
+
+    }
+
+    printf("function counting : %d\n", count_func);
+    printf("==================================\n");
+
+
+
 
     return 0;
 }
